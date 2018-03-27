43085c4d3e51d98a14c3d803ab143ef80b721700
eudaimos <jeff@hwgray.com>
2018-03-27 10:41:14 -0700

    chore(bootstrapping): bootstrapping jest with husky pre-commit hook to run tests

---
+++ config/jest.js
@@ -0,0 +1,36 @@
+import { shallow, render, mount } from "enzyme";
+
+export const setPathname = pathname => {
+  Object.defineProperty(window.location, "pathname", {
+    writable: true,
+    value: pathname
+  });
+};
+
+// Skip createElement warnings but fail tests on any other warning
+console.error = message => {
+  if (!/(React.createElement: type should not be null)/.test(message)) {
+    throw new Error(message);
+  }
+};
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+const localStorageMock = (function() {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    }
+  };
+})();
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
