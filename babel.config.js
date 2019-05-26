c682c76d6f6567035e283ff78a56cacb3488110a
eudaimos <jeff@hwgray.com>
2019-05-26 11:30:14 -0700

    chore(deps): added rollup + used lerna link convert
    affects: patois.api, patois.web, docs, @tao.js/koa, @tao.js/react, @tao.js/connect, @tao.js/feature,
    @tao.js/path, @tao.js/router, @tao.js/socket.io, @tao.js/core
    
    rollup will be used for building package code for distribution
    \`lerna link convert\` moved dev deps
    to root

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,15 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-transform-runtime',
-    '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties'
   ],
   env: {
     test: {
-      plugins: ['babel-plugin-istanbul']
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'babel-plugin-istanbul'
+      ]
     }
   }
 };
