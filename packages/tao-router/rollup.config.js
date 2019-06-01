94faa4588d8155ee342d8df48f2e8bb3ee37ffa6
eudaimos <jeff@hwgray.com>
2019-05-31 23:14:04 -0700

    feat(package): rollup build for tao-router complete (CJS + ESM)
    affects: @tao.js/router

--- packages/tao-router/rollup.config.js
+++ packages/tao-router/rollup.config.js
@@ -34,40 +34,46 @@ export default [
       json(),
       commonjs()
     ]
-  }
+  },
 
-  // // CommonJS (for Node) and ES module (for bundlers) build.
-  // // (We could have three entries in the configuration array
-  // // instead of two, but it's quicker to generate multiple
-  // // builds from a single configuration where possible, using
-  // // an array for the `output` option, where we can specify
-  // // `file` and `format` for each target)
-  // {
-  //   input: {
-  //     index: 'src/index.js'
-  //   },
-  //   output: [
-  //     {
-  //       dir: pkg.main,
-  //       format: 'cjs',
-  //       sourcemap: true,
-  //       exports: 'named'
-  //     },
-  //     {
-  //       dir: pkg.module,
-  //       format: 'esm',
-  //       sourcemap: true,
-  //       exports: 'named'
-  //     }
-  //   ],
-  //   plugins: [
-  //     babel({
-  //       runtimeHelpers: true,
-  //       exclude: ['node_modules/**']
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs()
-  //   ]
-  // }
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: {
+      index: 'src/index.js'
+    },
+    output: [
+      {
+        dir: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        dir: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    external: ['history'],
+    plugins: [
+      external(),
+      babel({
+        runtimeHelpers: true,
+        exclude: ['node_modules/**']
+      }),
+      external(),
+      resolve({
+        preferBuiltins: false,
+        browser: true
+      }),
+      json(),
+      commonjs()
+    ]
+  }
 ];
